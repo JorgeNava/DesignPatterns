d0c0cc1701cba11992eca2a20e4cdc69b4239157
Jorge Nava de la Peña <jorgenavadelapena@gmail.com>
2022-08-03 14:25:33 -0700

    Update Bridge.py

--- Bridge.py
+++ Bridge.py
@@ -1,89 +1,30 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
-
-
-class Abstraction:
-    """
-    The Abstraction defines the interface for the "control" part of the two
-    class hierarchies. It maintains a reference to an object of the
-    Implementation hierarchy and delegates all of the real work to this object.
-    """
-
-    def __init__(self, implementation: Implementation) -> None:
-        self.implementation = implementation
-
-    def operation(self) -> str:
-        return (f"Abstraction: Base operation with:\n"
-                f"{self.implementation.operation_implementation()}")
-
-
-class ExtendedAbstraction(Abstraction):
-    """
-    You can extend the Abstraction without changing the Implementation classes.
-    """
-
-    def operation(self) -> str:
-        return (f"ExtendedAbstraction: Extended operation with:\n"
-                f"{self.implementation.operation_implementation()}")
-
-
-class Implementation(ABC):
-    """
-    The Implementation defines the interface for all implementation classes. It
-    doesn't have to match the Abstraction's interface. In fact, the two
-    interfaces can be entirely different. Typically the Implementation interface
-    provides only primitive operations, while the Abstraction defines higher-
-    level operations based on those primitives.
-    """
-
-    @abstractmethod
-    def operation_implementation(self) -> str:
-        pass
-
-
-"""
-Each Concrete Implementation corresponds to a specific platform and implements
-the Implementation interface using that platform's API.
-"""
-
-
-class ConcreteImplementationA(Implementation):
-    def operation_implementation(self) -> str:
-        return "ConcreteImplementationA: Here's the result on the platform A."
-
-
-class ConcreteImplementationB(Implementation):
-    def operation_implementation(self) -> str:
-        return "ConcreteImplementationB: Here's the result on the platform B."
-
-
-def client_code(abstraction: Abstraction) -> None:
-    """
-    Except for the initialization phase, where an Abstraction object gets linked
-    with a specific Implementation object, the client code should only depend on
-    the Abstraction class. This way the client code can support any abstraction-
-    implementation combination.
-    """
-
-    # ...
-
-    print(abstraction.operation(), end="")
-
-    # ...
-
-
-if __name__ == "__main__":
-    """
-    The client code should be able to work with any pre-configured abstraction-
-    implementation combination.
-    """
-
-    implementation = ConcreteImplementationA()
-    abstraction = Abstraction(implementation)
-    client_code(abstraction)
-
-    print("\n")
-
-    implementation = ConcreteImplementationB()
-    abstraction = ExtendedAbstraction(implementation)
-    client_code(abstraction)
+class ProducingAPI1: 
+    def produceCuboid(self, length, breadth, height): 
+        print(f'API1 is producing Cuboid with length = {length}, '
+              f' Breadth = {breadth} and Height = {height}') 
+  
+class ProducingAPI2: 
+    def produceCuboid(self, length, breadth, height): 
+        print(f'API2 is producing Cuboid with length = {length}, '
+              f' Breadth = {breadth} and Height = {height}') 
+  
+class Cuboid: 
+    def __init__(self, length, breadth, height, producingAPI): 
+        self._length = length 
+        self._breadth = breadth 
+        self._height = height 
+        self._producingAPI = producingAPI 
+  
+    def produce(self): 
+        self._producingAPI.produceCuboid(self._length, self._breadth, self._height) 
+  
+    def expand(self, times): 
+        self._length = self._length * times 
+        self._breadth = self._breadth * times 
+        self._height = self._height * times 
+  
+cuboid1 = Cuboid(1, 2, 3, ProducingAPI1()) 
+cuboid1.produce() 
+  
+cuboid2 = Cuboid(19, 19, 19, ProducingAPI2()) 
+cuboid2.produce() 
